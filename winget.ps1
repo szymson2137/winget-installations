dbe614362d1dc301515a23139e3193e1a7fbe004
Szymon <103281923+szymson2137@users.noreply.github.com>
2024-08-27 12:12:50 +0200

    Update winget.ps1

--- winget.ps1
+++ winget.ps1
@@ -2,3 +2,7 @@ winget install Google.Chrome --scope machine --accept-source-agreements
 winget install brave.brave --scope machine
 winget install 7zip.7zip --scope machine
 winget install mozilla.firefox --scope machine
+winget install ApacheFriends.Xampp.8.2 --scope machine
+winget install Microsoft.VisualStudioCode --scope machine
+winget install Notepad++.Notepad++ --scope machine
+winget isttall Python.Python.3.12 --scope machine
