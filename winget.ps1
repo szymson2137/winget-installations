53799b43497d46699c15fca8d2bb0f29a5f8ca75
Szymon <103281923+szymson2137@users.noreply.github.com>
2024-08-27 10:21:12 +0200

    Create winget.ps1

---
+++ winget.ps1
@@ -0,0 +1,4 @@
+winget install Google.Chrome --scope machine --accept-source-agreements
+winget install brave.brave --scope machine
+winget install 7zip.7zip --scope machine
+winget install mozilla.firefox --scope machine
